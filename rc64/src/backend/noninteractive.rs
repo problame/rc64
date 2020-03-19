499577a6458a053c5f5b55be0ca7cc14c954c7bb
Joshua Bachmeier <joshua.bachmeier@student.kit.edu>
2020-03-20 00:20:10 +0100

    Add missing joystick impl for new backend

--- rc64/src/backend/noninteractive.rs
+++ rc64/src/backend/noninteractive.rs
@@ -1,4 +1,4 @@
-use crate::cia::{keyboard::KeyboardMatrix, PeripheralDevicesBackend};
+use crate::cia::{joystick::JoystickSwitch, keyboard::KeyboardMatrix, PeripheralDevicesBackend};
 
 pub struct NoninteractiveBackend;
 
@@ -9,5 +9,13 @@ pub fn new() -> NoninteractiveBackend {
 impl PeripheralDevicesBackend for NoninteractiveBackend {
     fn get_current_keyboard_matrix(&self) -> KeyboardMatrix {
         KeyboardMatrix::default()
+    }
+
+    fn get_current_joystick1_state(&self) -> JoystickSwitch {
+        JoystickSwitch::default()
+    }
+
+    fn get_current_joystick2_state(&self) -> JoystickSwitch {
+        JoystickSwitch::default()
     }
 }
